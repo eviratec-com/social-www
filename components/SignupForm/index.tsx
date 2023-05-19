d3ff440373cf8aab4d25f0cf1591cfd144f8d689
Callan Peter Milne <callanpmilne@gmail.com>
2023-05-20 00:04:19 +1000

    Fix Signup Form

--- components/SignupForm/index.tsx
+++ components/SignupForm/index.tsx
@@ -392,7 +392,7 @@ export default function SignupForm() {
               <Link href={`/privacy`} legacyBehavior>
                 <a target="_blank">Privacy Policy</a>
               </Link>
-              for Eviratec.net.
+              for EviratecSocial.
             </label>
 
             {touched(tosAcceptInputId) && true !== acceptLegal &&
